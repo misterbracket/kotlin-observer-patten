aac70df1710ab23c004d8b7e3b4bddd3502e265f
Max Klammer <maximilian.klammer@gmail.com>
2024-03-02 13:32:48 +0100

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,13 @@
 package org.example
 
-abstract class Observer {
-    abstract fun update(event: Event)
-    abstract fun getEventType(): String
+abstract class Observer(private val eventType: String) {
+
+    abstract fun process(event: Event): String
+
+    fun getEventType(): String {
+        return eventType
+    }
+
     fun sendSegmentEvent(eventName: String) {
         println("Sending SegmentEvent for event: $eventName")
     }
@@ -12,7 +17,6 @@ data class Event(val type: String)
 
 interface Observable {
     fun addObserver(observer: Observer)
-    fun removeObserver(observer: Observer)
     fun notifyObservers(event: Event)
 }
 
@@ -23,13 +27,9 @@ class EmailManager : Observable {
         observers.add(observer)
     }
 
-    override fun removeObserver(observer: Observer) {
-        observers.remove(observer)
-    }
-
     override fun notifyObservers(event: Event) {
         for (observer in observers) {
-            observer.update(event)
+            observer.process(event)
         }
     }
 
@@ -37,33 +37,43 @@ class EmailManager : Observable {
         // Notify only observers interested in this event type
         val filteredObservers = observers.filter { it.getEventType() == event.type }
         for (observer in filteredObservers) {
-            observer.update(event)
+            //Segment Events can be returned from the process method
+            //and passed into the sendSegmentEvent method
+            val segmentEvent = observer.process(event)
+            observer.sendSegmentEvent(segmentEvent)
         }
+
     }
 }
 
-class KafkaEventObserver(private val name: String, private val eventType: String) : Observer() {
-    override fun update(event: Event) {
+class SignUpEventObserver(private val name: String) : Observer(eventType = "signup") {
+
+    override fun process(event: Event): String {
         println("$name received event: ${event.type}")
         // Implement logic specific to this email
         // For example, fetch some data, do some transformation on event data etc., create the correct format for Segment Event
         // ...
-        when (event.type) {
-            "signup" -> {
-                println("Processing Event: ${event.type}. Custom logic is being executed for this event type")
-            }
-
-            "fdd" -> {
-                println("Processing Event: ${event.type}. Custom logic is being executed for this event type")
-            }
-        }
-
-        this.sendSegmentEvent(event.type)
+        println("Processing Event: ${event.type}. Custom logic is being executed for this event type")
+        val segmentEvent = "signupSegmentEvent"
+        return segmentEvent
     }
-
-    override fun getEventType(): String = eventType
-
 }
+
+
+class FDDEventObserver(private val name: String) : Observer(eventType = "fdd") {
+
+    override fun process(event: Event): String {
+        println("$name received event: ${event.type}")
+        // Implement logic specific to this email
+        // For example, fetch some data, do some transformation on event data etc., create the correct format for Segment Event
+        // ...
+        println("Processing Event: ${event.type}. Custom logic is being executed for this event type")
+        // would be great if we can return the Segment Event from here
+        val segmentEvent = "fddSegmentEvent"
+        return segmentEvent
+    }
+}
+
 
 fun main() {
     while (true) {
@@ -80,8 +90,8 @@ fun main() {
         val emailManager = EmailManager()
 
         // Create observers
-        val signupEventObserver = KafkaEventObserver("Signup Event Observer", "signup")
-        val fddEventObserver = KafkaEventObserver("FDD Event Observer", "fdd")
+        val signupEventObserver = SignUpEventObserver("Signup Event Observer")
+        val fddEventObserver = FDDEventObserver("FDD Event Observer")
 
         // Register observers with the observable
         emailManager.addObserver(signupEventObserver)
